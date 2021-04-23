054f6b79fc82a4bc4540edf100ac67bd0cbba50f
joaohitallo <joaohitallo97@gmail.com>
2021-04-22 22:50:42 -0300

    Segundo dia NLW#5

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,36 +1,57 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, ColorPropType} from 'react-native';
-
+import { SafeAreaView, 
+    Text, 
+    Image, 
+    TouchableOpacity, 
+    StyleSheet, 
+    Dimensions,
+    View,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons'
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
-import { Button } from '../components/buttons';
-import { useState } from 'react';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+
 
 export function Welcome(){
-    const [visible, setVisible] = useState(false);
 
-    function handleVisibility(){
-        setVisible(true)
+    const navigation = useNavigation();
+
+    function handleStart(){
+        navigation.navigate('UserIdentification');
     }
-
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil 
-            </Text>
-            {
-            visible &&
-            <Image source={wateringImg} />
-            }
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.   
-            </Text>
-            
-            <Button title=">" />
-            
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de{'\n'}
+                    forma fácil 
+                </Text>
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode='contain'
+                />
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.   
+                </Text>
+                
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    <Text >
+                        <Feather 
+                            name="chevron-right" 
+                            style={styles.buttonIcon} 
+                        />
+                    </Text>
+                </TouchableOpacity>
+                </View>
         </SafeAreaView>
 
     )
@@ -40,21 +61,33 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around'
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text,
     },
+    image: { 
+        height: Dimensions.get('window').width * 0.7,
+    }, 
     button: {
         backgroundColor: colors.green,
         justifyContent: 'center',
@@ -62,14 +95,10 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        paddingHorizontal: 10
+        width: 56,    
     },
-    image: {
-        width: 292,
-        height: 284,
-    }, 
-    buttontext: {
+    buttonIcon: {
         color: colors.white,
-        fontSize: 24
+        fontSize: 32,
     }
 })
